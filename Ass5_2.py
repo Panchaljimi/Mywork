9360ba9e8e52526172e9fc5b5259f34c369e8f74
Panchaljimi <50333162+Panchaljimi@users.noreply.github.com>
2019-06-04 11:05:43 -0700

    Add files via upload

---
+++ Ass5_2.py
@@ -0,0 +1,8 @@
+#Prg:2 Write a higher order function to find out the reduced output of the
+#range(1,10) after using an association function as multiplication.
+
+from functools import reduce
+list=range(1,10)
+res=reduce((lambda p,q:p*q),list)
+#res=map(lambda n:n*n,range(10))
+print("The Reduce result is:",res)
